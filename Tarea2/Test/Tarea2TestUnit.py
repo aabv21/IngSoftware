b83b09c73b10f4a20d35917862081f387be54f0f
esft24 <12-11555@usb.ve>
2017-10-04 15:36:21 -0400

    Add files via upload

--- Tarea2/Test/Tarea2TestUnit.py
+++ Tarea2/Test/Tarea2TestUnit.py
@@ -7,18 +7,29 @@ from Tarea2.Tarea2 import *
 class TestTarea(unittest.TestCase):
     
     #Tests de funcionamiento
-    #Tests funcipn Tiempo de Trabajo
+    #Tests funcion Tiempo de Trabajo
 
     def test_TiempoDeTrabajo(self):
-        # Se calcularan 20 horas de trabajo
+        #Miercoles a las 3 hasta sabado a las 23.
         InicioServicio = datetime(2017, 5, 31, hour=3, minute=20, second=0)
-        FinalServicio = datetime(2017, 6, 3, hour=23, minute=0, second=0) #Notese los 20 minutos faltantes
+        FinalServicio = datetime(2017, 6, 3, hour=23, minute=0, second=0)
         
         result = tiempoDeTrabajo(InicioServicio, FinalServicio)
         
-        p = str(result)
+        #69 horas semana + 23 fin de semana
+        self.assertEqual(result, [69, 23], msg="La prueba no fue satisfactoria")
         
-        self.assertEqual(result, [69, 23], msg=p)
+        
+        #Viernes a las 23:59:59 hasta Sabado a las 0:59:59
+        InicioServicio2 = datetime(2017, 10, 6, hour=23, minute=59, second=59)
+        FinalServicio2 = datetime(2017, 10, 7, hour=0, minute=59, second=59)
+        
+        result2 = tiempoDeTrabajo(InicioServicio2, FinalServicio2)
+        
+        #1 hora de semana, a pesar de que solo se tomo 1 segundo del dia de semana
+        self.assertEqual(result2, [1, 0], msg="La prueba no fue satisfactoria")
+    
+    
 
 if __name__ == "__main__":
     unittest.main() 
